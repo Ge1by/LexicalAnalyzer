581e8015f8720bad7f62cbc635d73d92c3450f75
Ge1by <pushin00@gmail.com>
2023-01-30 13:37:26 +0400

    Add project files.

---
+++ LAB1/FormMain.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace LAB1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxInput = new System.Windows.Forms.RichTextBox();
+            this.buttonAnalyze = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridViewRecognizedTokens = new System.Windows.Forms.DataGridView();
+            this.richTextBoxMessages = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Токен = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRecognizedTokens)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBoxInput
+            // 
+            this.richTextBoxInput.Location = new System.Drawing.Point(22, 109);
+            this.richTextBoxInput.Name = "richTextBoxInput";
+            this.richTextBoxInput.Size = new System.Drawing.Size(365, 256);
+            this.richTextBoxInput.TabIndex = 1;
+            this.richTextBoxInput.Text = "2.5 %~ комментарий ~%    fab 26.32  t2   32     %~ 35  q_etr   \nhg ; 23 ~%    sdg" +
+    "     3.523\nk3j %~\n\nLL\n\n~%\ne4q_e  2456.324\n0";
+            // 
+            // buttonAnalyze
+            // 
+            this.buttonAnalyze.Location = new System.Drawing.Point(142, 384);
+            this.buttonAnalyze.Name = "buttonAnalyze";
+            this.buttonAnalyze.Size = new System.Drawing.Size(137, 34);
+            this.buttonAnalyze.TabIndex = 2;
+            this.buttonAnalyze.Text = "Анализировать текст";
+            this.buttonAnalyze.UseVisualStyleBackColor = true;
+            this.buttonAnalyze.Click += new System.EventHandler(this.buttonAnalyze_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Входной текст:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(457, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Слова первого типа: числа (целые и вещественные), например  43,  5.2,  1,  23.1, " +
+    " 45.234";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(782, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Слова второго типа: идентификаторы (состоят из букв, цифр и символов подчеркивани" +
+    "я, начинаются с буквы), например  x,  MyFunc_1,  z0,  Var_3,  ddd";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(234, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Комментарий: многострочный  %~  ........  ~%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(405, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Распознанные токены:";
+            // 
+            // dataGridViewRecognizedTokens
+            // 
+            this.dataGridViewRecognizedTokens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewRecognizedTokens.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Токен,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridViewRecognizedTokens.Location = new System.Drawing.Point(408, 109);
+            this.dataGridViewRecognizedTokens.Name = "dataGridViewRecognizedTokens";
+            this.dataGridViewRecognizedTokens.ReadOnly = true;
+            this.dataGridViewRecognizedTokens.Size = new System.Drawing.Size(393, 256);
+            this.dataGridViewRecognizedTokens.TabIndex = 9;
+            // 
+            // richTextBoxMessages
+            // 
+            this.richTextBoxMessages.Location = new System.Drawing.Point(22, 445);
+            this.richTextBoxMessages.Name = "richTextBoxMessages";
+            this.richTextBoxMessages.Size = new System.Drawing.Size(779, 83);
+            this.richTextBoxMessages.TabIndex = 10;
+            this.richTextBoxMessages.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 429);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Сообщения:";
+            // 
+            // Токен
+            // 
+            this.Токен.HeaderText = "№";
+            this.Токен.Name = "Токен";
+            this.Токен.ReadOnly = true;
+            this.Токен.Width = 30;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Токен";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Тип токена";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Индекс строки";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 60;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Индекс символа";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 60;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 541);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBoxMessages);
+            this.Controls.Add(this.dataGridViewRecognizedTokens);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAnalyze);
+            this.Controls.Add(this.richTextBoxInput);
+            this.Name = "FormMain";
+            this.Text = "Лабораторная работа № 1 - Разработка лексического анализатора";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRecognizedTokens)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxInput;
+        private System.Windows.Forms.Button buttonAnalyze;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridViewRecognizedTokens;
+        private System.Windows.Forms.RichTextBox richTextBoxMessages;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Токен;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
